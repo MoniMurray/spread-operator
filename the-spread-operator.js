2984b9aadc4eed9c5a061da53ee77bb0bb53b94b
Monica Murray <Monica.rickerby@gmail.com>
2023-01-05 16:18:50 +0000

    Initial spread operator commit, trial and error

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,8 +4,21 @@
  */
 
 // No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(8);
+console.log(arr2);
+console.log(arr1);
+arr1.pop(3);
+console.log(arr2);
+arr2.splice(2);
+console.log(arr2);
 
 // Copying an array
+let arr3 = [1, 2, 3];
+let arr4 = [...arr3, 9, 8];
+console.log(arr3);
+console.log(arr4);
 
 // Copying an object
 
